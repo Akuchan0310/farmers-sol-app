f431ac9dc610f7ca48ef619a2cdebbed1b9ecaa0
sanjana-19o8 <sanjanavajrapu19@gmail.com>
2023-09-17 23:55:06 +0530

    asdf

--- client/helpers/context.tsx
+++ client/helpers/context.tsx
@@ -153,6 +153,8 @@ export const SupplyChainProvider = ({ children }: any) => {
                 addFarmer,
                 addRetailer,
                 addConsumer,
+                fetchProductDetails,
+                fetchOriginDidi,
                 connectWallet,
             }}
         >
